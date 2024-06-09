f0ba33db3ee93b7742c0a4497c23bffcf4c4875c
IanPatrickMTan <ianparicktan@gmail.com>
2024-06-09 18:31:00 -0400

    made a small test and fixed some small bugs which prevented the code from running and now the memory block returns a tensor which looks like the right shape but I am not sure if the calculations are right, the fastest way would be to train or maybe test piece by piece which could be a bit of a pain

--- src/model.py
+++ src/model.py
@@ -9,7 +9,7 @@ def normalize(x):
 
 @pt.compile
 def mix_weight(x, w):
-  out = pt.einsum('...k, ik, i...k', x, w)
+  out = pt.einsum('...k, ik -> i...k', x, w)
   return out
 
 @pt.compile
@@ -31,13 +31,7 @@ def weight_only(x, w):
   return out
 
 @pt.compile
-def dense_norm(x, w, b):
-  out = pt.nn.functional.linear(x, w, b)
-  norm_out = normalize(out)
-  return norm_out
-
-@pt.compile
-def rkv_layer(x, last_x, mix_w, rkv_w):
+def rkv_block(x, last_x, mix_w, rkv_w):
   mix_x = mix(x, last_x, mix_w)
   r_x, k_x, v = multi_weight_only(mix_x, rkv_w)
 
@@ -55,13 +49,12 @@ def mem_out_block(mem, r, out_w):
   out = weight_only(r_mem, out_w)
   return out
 
-
 @pt.compile
-def serial_memory(x, last_x, last_mem, mix_w, rkv_w, out_w, raw_decay):
+def serial_memory(x, last_x, last_mem, mix_w, rkv_w, out_w, decay):
   k_kv, r = rkv_block(x, last_x, mix_w, rkv_w)
 
-  decay = pt.exp(-pt.exp(raw_decay))
-  mem = last_mem * decay + k_kv
+  mem_decay = pt.exp(-pt.exp(decay))
+  mem = last_mem * mem_decay + k_kv
 
   out = mem_out_block(mem, r, out_w)
   return out, x, mem
@@ -76,29 +69,30 @@ def mem_scan(last_mem, decay, k_kv):
   return mem, last_mem
 
 @pt.compile
-def parallel_memory(x, raw_last_x, last_mem, mix_w, rkv_w, out_w, raw_decay):
-  last_x = pt.zeros_like(x)
-  last_x[:, 0] = raw_last_x
-  last_x[:, 1:] = x[:, :-1]
+def parallel_memory(x, last_x, last_mem, mix_w, rkv_w, out_w, decay):
+  all_last_x = pt.zeros_like(x)
+  all_last_x[:, 0] = last_x
+  all_last_x[:, 1:] = x[:, :-1]
   new_x = x[:, -1]
-  k_kv, r = rkv_block(x, last_x, mix_w, rkv_w)
+  k_kv, r = rkv_block(x, all_last_x, mix_w, rkv_w)
 
-  decay = pt.exp(-pt.exp(raw_decay))
-  mem, new_mem = mem_scan(last_mem, decay, k_kv)
+  mem_decay = pt.exp(-pt.exp(decay))
+  mem, new_mem = mem_scan(last_mem, mem_decay, k_kv)
 
   out = mem_out_block(mem, r, out_w)
   return out, new_x, new_mem
 
 class DenseNorm(pt.nn.Module):
   def __init__(self, in_len, out_len):
-    self.dense = pt.nn.linear(in_len, out_len)
+    super().__init__()
+    self.dense = pt.nn.Linear(in_len, out_len)
 
   def forward(self, x):
     x = self.dense(x)
     x = normalize(x)
     return x
 
-class Memory(pt.nn.Module):
+class MemoryBlock(pt.nn.Module):
   def __init__(self, in_len, mem_len=None, out_len=None, serial=False, last_x=None, last_mem=None, mix_w=None, rkv_w=None, decay=None, out_w=None):
     super().__init__()
     
@@ -112,19 +106,19 @@ class Memory(pt.nn.Module):
     self.decay = decay if decay else pt.nn.Parameter(pt.randn(self.mem_len))
     self.out_w = out_w if out_w else pt.nn.Parameter(pt.randn(self.in_len, self.mem_len))
 
-    self.dense_norm = DenseNorm(in_len, in_len)
-    self.dense = pt.nn.linear(in_len, out_len)
+    self.dense_norm = DenseNorm(self.in_len, self.in_len)
+    self.dense = pt.nn.Linear(self.in_len, self.out_len)
     self.gelu = pt.nn.GELU()
     
-    self.memory = serial_memory if serial else memory
+    self.memory = serial_memory if serial else parallel_memory
     self.last_x = last_x if last_x else pt.zeros(1, self.in_len)
     self.last_mem = last_mem if last_mem else pt.zeros(2, 1, self.mem_len)
   
   def forward(self, x):
     x = self.dense_norm(x)
-    dx, self.last_x, self.last_mem = self.memory(x)
+    dx, self.last_x, self.last_mem = self.memory(x, last_x=self.last_x, last_mem=self.last_mem, mix_w=self.mix_w, rkv_w=self.rkv_w, out_w=self.out_w, decay=self.decay)
     x = x + dx
-    x = dense(x)
+    x = self.dense(x)
     x = self.gelu(x)
     return x
 
@@ -133,10 +127,18 @@ class Memory(pt.nn.Module):
     self.last_mem = pt.zeros(2, 1, self.mem_len)
 
   def set_serial(self, serial):
-    self.memory = serial_memory if serial else memory
+    self.memory = serial_memory if serial else parallel_memory
 
   def get_state(self):
     return {
         'last_x': self.last_x,
         'last_mem': self.last_mem,
         }
+
+if __name__ == '__main__':
+  TORCH_LOGS="+dynamo"
+  TORCHDYNAMO_VERBOSE=1
+  print('Running test...')
+  l = MemoryBlock(8, serial=1)
+  i = pt.randn(16, 8)
+  o = l(i)
